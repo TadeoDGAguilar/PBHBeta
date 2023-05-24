87f74f3fd31d9df942a1a1d3ca92e033f07ccc0b
Tadeo D. Aguilar <76193379+TadeoDGAguilar@users.noreply.github.com>
2023-05-23 22:44:35 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.1'
+PACKAGE_NAME = 'PBHBeta'
+URL = 'https://github.com/TadeoDGAguilar'
+LICENSE = 'GNU GPL'
+DESCRIPTION = 'Library to compute abundances to PBHs in different scenarios on Early Universe'
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = "text/markdown"
+INSTALL_REQUIRES = [
+      'matplotlib',
+      'numpy',
+      'scipy',
+      ]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=['Tadeo D. GA', 'Luis E. PA'],
+    maintainer = 'Tadeo D. Gómez Aguilar',
+    maintainer_email = 'tadeo.daguilar@gmail.com',
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)

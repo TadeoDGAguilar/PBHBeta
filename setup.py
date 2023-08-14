4b782713638cef966df69b636c1089587d2dd118
Tadeo D. Gomez-Aguilar <76193379+TadeoDGAguilar@users.noreply.github.com>
2023-08-14 03:13:07 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0'
+VERSION = '1.0.1'
 PACKAGE_NAME = 'PBHBeta'
-URL = 'https://github.com/TadeoDGAguilar'
+URL = 'https://github.com/TadeoDGAguilar/PBHBeta'
 LICENSE = 'GNU GPL'
-DESCRIPTION = 'Library to compute abundances to PBHs in different scenarios on Early Universe'
+DESCRIPTION = 'PBHBeta, a code to explore the constraints on PBH abundances and power spectrum.'
 LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
 LONG_DESC_TYPE = "text/markdown"
 INSTALL_REQUIRES = [

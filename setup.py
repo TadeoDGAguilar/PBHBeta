7238e5ba8b789db3fab3e63df432cc923c0730f9
Tadeo D. Gomez-Aguilar <76193379+TadeoDGAguilar@users.noreply.github.com>
2024-08-01 03:33:11 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.0.1'
+VERSION = '1.0.5'
 PACKAGE_NAME = 'PBHBeta'
 URL = 'https://github.com/TadeoDGAguilar/PBHBeta'
 LICENSE = 'GNU GPL'
